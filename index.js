8bd9e0e148cd2e21a9dd5945d4d7f4567d4c430c
Yimin Du <d.yimin@gmail.com>
2016-07-15 06:18:14 +0930

    v1.0.0

---
+++ index.js
@@ -0,0 +1,65 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+var mongodb = require('mongodb');
+var mongoClient = mongodb.MongoClient;
+var dburl = process.env.MONGO_URI;
+
+app.get('/:param', (req, res) => {
+	var param = req.params.param;
+	if(isNaN(param)) {
+		// create a new db doc: { url: param, id: randomNum }, and update db
+		mongoClient.connect(dburl, (err, db) => {
+			if(err) {
+				res.send(500);
+			} else {
+				var id = Math.round(Math.random() * 9999 + 1);
+				db.collection('urlmap').updateOne(
+					{ url: param },
+					{
+						$set: { id: id },
+						$currentDate: { lastModified: true }
+					},
+					{ upsert: true }
+				);
+				db.close();
+
+				// return { "original_url": param, "shortened_url": shortUrl }
+				var shortUrl = "http://localhost:8080/" + id;
+				res.send({
+					"original_url": param, 
+					"shortened_url": shortUrl
+				});
+			}
+
+		})
+	} else {
+		console.log("a num: ", param);
+		// query { id: param } to db
+		// if success, redirect to url; else return error
+		mongoClient.connect(dburl, (err, db) => {
+			if(err) {
+				res.send(500);
+			} else {
+				console.log("query: ", { id: Number(param) });
+				db.collection('urlmap').find({ id: Number(param) }).toArray((err, items) => {
+					if(items.length > 0) {
+						console.log(items[0].url);
+						res.redirect(301, 'http://' + items[0].url);
+					} else {
+						res.send(404);
+					}
+
+				});
+			}
+
+			db.close();
+		})
+	}
+});
+
+
+
+
+
+app.listen(port);
